6ccc26a7d6034b8f26527923a3512df6cd639deb
Hrishabh Kumar <krhrishabh14@gmail.com>
2026-07-25 10:51:14 +0530

    Update cpu_gemm.cpp

--- cpu_baseline/cpu_gemm.cpp
+++ cpu_baseline/cpu_gemm.cpp
@@ -1,1 +1,14 @@
+#include <omp.h>
 
+void cpu_gemm_omp(const float* A, const float* B, float* C, int N) {
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            float sum = 0.0f;
+            for (int k = 0; k < N; ++k) {
+                sum += A[i * N + k] * B[k * N + j];
+            }
+            C[i * N + j] = sum;
+        }
+    }
+}
